64bd8d0ca58e3f9b11658281d7c64ed3117fbcc0
Louraes202 <peterlouraes@gmail.com>
2024-02-29 16:11:14 +0000

    .

--- src/screens/Tasks/Tabs/TasksTab.js
+++ src/screens/Tasks/Tabs/TasksTab.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from '../../../styles/styles'; // Importar estilos globais
+import * as SQLite from 'expo-sqlite';
 
 
 const TasksTab = () => {
     return (
       <View style={styles.screen}>
         <Text style={styles.maintext}>Tasks Screen</Text>
+        <Text style={styles.maintext}></Text>
       </View>
     )
   }
