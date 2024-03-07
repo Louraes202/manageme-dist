611d653a905852670686488be9a69a9410c35f76
Louraes202 <peterlouraes@gmail.com>
2024-03-07 16:36:01 +0000

    Fixed menu

--- src/screens/Tasks/Tabs/TasksTab.js
+++ src/screens/Tasks/Tabs/TasksTab.js
@@ -2,24 +2,65 @@ import React from "react";
 import { useState } from "react";
 import { View, Text } from "react-native";
 import styles from "../../../styles/styles"; // Importar estilos globais
-import { Fab, Center } from "native-base";
+import {
+  Fab,
+  Center,
+  Flex,
+  VStack,
+  Menu,
+  Box,
+  HamburgerIcon,
+  Pressable,
+} from "native-base";
 import { FontAwesome5 } from "@expo/vector-icons";
-import Colors from '../../../../assets/utils/pallete.json';
+import Colors from "../../../../assets/utils/pallete.json";
+import Task from "../components/Task";
+import { ScrollView } from "react-native-gesture-handler";
 
 const TasksTab = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title_text}>Tasks Screen</Text>
+      <ScrollView>
+        <VStack py="">
+          <Flex direction="column">
+            <Task
+              name="testa"
+              desc="tesasdasdasdasdasdasdsad asdasdsd asdasdads asdasdt"
+              group="test"
+            ></Task>
+          </Flex>
+        </VStack>
+      </ScrollView>
 
       {/* Footer button */}
-      <View style={{ position: "absolute", left: 0, right: 0, bottom: 5 }}>
-        <Fab
-          renderInPortal={false}
-          shadow={2}
-          size="sm"
-          icon={<FontAwesome5 name="plus" color={"white"} size={24} underlayColor={Colors.navblue}/>}
-        />
-      </View>
+
+      <Box>
+        <Menu
+          w="190"
+          trigger={(triggerProps) => {
+            return (
+              <Fab
+                accessibilityLabel="More options menu"
+                {...triggerProps}
+                icon={
+                  <FontAwesome5
+                    name="plus"
+                    color={"white"}
+                    size={24}
+                    underlayColor={Colors.navblue}
+                  />
+                }
+                style={{marginBottom: 50}}
+              ></Fab>
+            );
+          }}
+        >
+          <Menu.Item>New task</Menu.Item>
+          <Menu.Item>New group</Menu.Item>
+          <Menu.Item>New category</Menu.Item>
+        </Menu>
+      </Box>
     </View>
   );
 };
---
+++ src/screens/Tasks/components/Task.js
@@ -0,0 +1,50 @@
+import { Box, Pressable, HStack, Badge, Text, Flex, Spacer } from "native-base";
+import { useState, useffect } from "react";
+import * as SQLite from "expo-sqlite";
+
+const Task = ({name, desc, group, category, date}) => {
+  return (
+    <Box alignItems="center" marginY={2}>
+      <Pressable
+        onPress={() => console.log("I'm Pressed")}
+        rounded="8"
+        overflow="hidden"
+        borderWidth="1"
+        borderColor="coolGray.300"
+        maxW="96"
+        shadow="3"
+        bg="coolGray.100"
+        p="5"
+        w="96"
+      >
+        <Box>
+          <HStack alignItems="center">
+            <Badge
+              colorScheme="darkBlue"
+              _text={{
+                color: "white",
+              }}
+              variant="solid"
+              rounded="4"
+            >
+              { group }
+            </Badge>
+            <Spacer />
+            <Text fontSize={10} color="coolGray.800">
+              1 month ago
+            </Text>
+          </HStack>
+          <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
+            { name }
+          </Text>
+          <Text mt="2" fontSize="sm" color="coolGray.700">
+            { desc }
+          </Text>
+
+        </Box>
+      </Pressable>
+    </Box>
+  );
+};
+
+export default Task;
