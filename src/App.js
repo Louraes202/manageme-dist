d717159f7b9e1b84e90935c3dd4d7c07819f4a42
Martim Loureiro <peterlouraes@gmail.com>
2024-04-14 19:34:17 +0100

    Fixed merge issue

--- src/App.js
+++ src/App.js
@@ -17,32 +17,6 @@ import { NativeBaseProvider, extendTheme } from "native-base";
 import createTablesQuery from "./services/SQLite/createQuery";
 
 
-const theme = extendTheme({
-  colors: {
-    // Add new color
-    primary: {
-      50: "#E3F2F9",
-      100: "#C5E4F3",
-      200: "#A2D4EC",
-      300: "#7AC1E4",
-      400: "#47A9DA",
-      500: "#0088CC",
-      600: "#007AB8",
-      700: "#006BA1",
-      800: "#005885",
-      900: "#003F5E",
-    },
-    // Redefining only one shade, rest of the color will remain same.
-    amber: {
-      400: "#d97706",
-    },
-  },
-  config: {
-    // Changing initialColorMode to 'dark'
-    initialColorMode: "dark",
-  },
-});
-
 const LoadingScreen = ({ styles }) => (
   <View style={styles.splash_container}>
     <Image source={require("../img/mylogo.png")} style={styles.mainlogo} />
@@ -102,7 +76,7 @@ const App = () => {
   const Drawer = createDrawerNavigator();
 
   return (
-    <NativeBaseProvider theme={theme}>
+    <NativeBaseProvider>
       <NavigationContainer>
         <Stack.Screen
           name="Auth"
